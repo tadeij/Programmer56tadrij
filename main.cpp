9259d373988a5f2efb68616f21fcfad92e27629b
tadeij <64397645+tadeij@users.noreply.github.com>
2020-04-29 08:27:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -42,3 +42,20 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmsShow)
         DispatchMessage(&msg);
     }
 }
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+{
+    switch(msg)
+    {
+         case WM_CLOSE:
+             DrstroyWindow(hwnd);
+             break;
+         case WM_DESTROY:
+             PostQuitMessage(0);
+             break;
+         default:
+             return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+   
+    return 0;
+}
